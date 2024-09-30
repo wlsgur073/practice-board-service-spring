0135e39894241189baf92ebb20d2905dfe66c20c
wlsgur073 <wlsgur073@gmail.com>
2024-09-30 19:26:12 +0900

    #7 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    
    기술 :
    * 스프링 웹
    * 액츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/test/java/com/board/boardpractice/BoardPracticeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.boardpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardPracticeSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
