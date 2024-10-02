e2bb0e69174f2deaef24dcd43dee4fc4633da91c
wlsgur073 <wlsgur073@gmail.com>
2024-10-02 12:24:15 +0900

    #18 - 회원 계정 도메인 구현 및 erd 반영
    `UserAccount` 이름으로 회원 계정 도메인 생성
    `user`, `account` 등은 mysql 예약어이므로 피함
    필드명도 mysql 예약어를 의식하여 정함
    
    테스트 데이터에 임의 계정 1개 등록
    테스트용이지만 패스워드가 노출되는 방식이므로 고민 필요
    
    회원 정보는 API 로 노출되지 않았으면 한다.
    이에 테스트에 404 not found 를 확인하는 테스트 추가
    간단하게 모든 http 메소드에 대응

---
+++ src/main/java/com/board/boardpractice/repository/UserAccountRepository.java
@@ -0,0 +1,7 @@
+package com.board.boardpractice.repository;
+
+import com.board.boardpractice.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,8 @@
+-- 테스트 계정
+-- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
+insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
+    ('tester', 'asdf1234', 'tester', 'tester@mail.com', 'I am tester.', now(), 'tester', now(), 'tester');
+
 
 -- 123 게시글
 insert into article (title, content, hashtag, created_by, modified_by, created_at, modified_at) values ('Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.', 'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.', null, 'Max', 'IntegerAc.mp3', '2023-03-07 19:00:54', '2023-03-13 16:14:47');
