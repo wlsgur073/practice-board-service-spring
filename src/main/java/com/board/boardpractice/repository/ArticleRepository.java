08df4fb620c1448c7fe597c285288e442d5e62a4
wlsgur073 <wlsgur073@gmail.com>
2024-10-03 20:51:19 +0900

    #20 - 비즈니스 로직 구현
    테스트를 토대로 게시판 서비스에 필요한 비즈니스 로직 구현 완료

--- src/main/java/com/board/boardpractice/repository/ArticleRepository.java
+++ src/main/java/com/board/boardpractice/repository/ArticleRepository.java
@@ -18,7 +18,18 @@ public interface ArticleRepository extends
         QuerydslPredicateExecutor<Article>, // 엔티티에 몯느 필드에 대한 기본 검색 기능 추가 // 전체 검색밖에 안됨
         QuerydslBinderCustomizer<QArticle> // 부분 검색 등 부가 기능을 만들기 위해
 {
-    Page<Article> findByTitle(String title, Pageable pageable);
+    Page<Article> findByTitleContaining(String title, Pageable pageable);
+
+    // Containing은 부분 검색을 의미한다.
+    Page<Article> findByContentContaining(String content, Pageable pageable);
+
+    // Article 안에 UserAccount가 필요하기에 이와 같이 네이밍함.
+    Page<Article> findByUserAccount_UserIdContaining(String userId, Pageable pageable);
+
+    // Containing하면 %% 와일드카드로 인하여 인덱싱을 못하게 될 수 있지만, 나중에 튜닝 요소로 놔두기로 하자.
+    Page<Article> findByUserAccount_NicknameContaining(String nickname, Pageable pageable);
+
+    Page<Article> findByHashtag(String hashtag, Pageable pageable); // 해시태그는 정확한 검색으로 설정
 
     @Override
     default void customize(QuerydslBindings bindings, QArticle root) {
