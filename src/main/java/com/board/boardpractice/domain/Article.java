a4233067d0ac4a916f1e80035e3e1eab90a3ae6f
wlsgur073 <wlsgur073@gmail.com>
2024-10-03 16:25:35 +0900

    #21 - 회원 계정 엔티티와 게시글, 댓글에 연관관계 적용
    테이블 필드 수정으로 영향 받은
    `data.sql` 테스트 데이터도 모두 업데이트
    
    하는 김에 게시글 -> 댓글 컬렉션의 정렬을 최신 순으로 적용 및 불필요한 주석 제거.

--- src/main/java/com/board/boardpractice/domain/Article.java
+++ src/main/java/com/board/boardpractice/domain/Article.java
@@ -10,14 +10,13 @@ import java.util.Objects;
 import java.util.Set;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "title"),
         @Index(columnList = "hashtag"),
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy"),
 })
-//@EntityListeners(AuditingEntityListener.class) // -> MappedSuperclass 클래스에 배치가 가능
 @Entity
 public class Article extends AuditingFields {
 
@@ -35,35 +34,27 @@ public class Article extends AuditingFields {
     @Setter
     private String hashtag;
 
+    @Setter @ManyToOne(optional = false) private UserAccount userAccount;
+
     // 양방향 설정
-    @ToString.Exclude // 양방향 바인딩으로 인한 순환 참조(무한루프) 제외 처리
-    @OrderBy("id")
-    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL) // 댓글은 게시글에 속해야 되니까 cascade
+    @ToString.Exclude
+    @OrderBy("createdAt DESC")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
-
-    // metadata EnableJpaAuditing 설정
-//    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;
-//    @CreatedBy @Column(nullable = false, length = 100) private String createdBy;
-//    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;
-//    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;
-
-    // 중복되는 column을 추출하는 방법은 2가지가 있다.
-//    @MappedSuperclass : AuditingFields를 extends해서 사용
-//    @Embedded : 공통 컬럼들을 모은 클래스를 하나의 필드로 설정하는 방법
-//    당연하지만 주의사항으로 컬럼을 공통으로 사용하면 장단점이 있을 것이다.
 
     protected Article() {
     } // 외부에서 생성자 생성 못하게 설정
 
-    private Article(String title, String content, String hashtag) {
+    private Article(UserAccount userAccount, String title, String content, String hashtag) {
+        this.userAccount = userAccount;
         this.title = title;
         this.content = content;
         this.hashtag = hashtag;
     }
 
     // factory method 패턴
-    public static Article of(String title, String content, String hashtag) {
-        return new Article(title, content, hashtag);
+    public static Article of(UserAccount userAccount, String title, String content, String hashtag) {
+        return new Article(userAccount, title, content, hashtag);
     }
 
     // 동일성 동등성 검사 - 모든 컬럼을 비교할 필요 없이 ID만 보면 되니, custom
