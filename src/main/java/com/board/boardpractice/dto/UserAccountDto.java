47e5051bcec50220da02f28528f26a4144a8e170
wlsgur073 <wlsgur073@gmail.com>
2024-10-07 21:34:16 +0900

    #41 - 리팩토링: UserAccountDto 에 메타 정보를 넣지 않는 of() 팩토리 메소드 추가
    테스트를 용이하게 하기 위해 추가함
    
    테스트 목적 외에도,
    repository 레이어에 영속되지 않은 객체는
    메타 정보가 없는 것이 당연하므로
    새로운 데이터를 만들 때 이 부분을 입력하지 않아도 되는
    팩토리 메소드를 만들어주는 것이 의미가 있음

--- src/main/java/com/board/boardpractice/dto/UserAccountDto.java
+++ src/main/java/com/board/boardpractice/dto/UserAccountDto.java
@@ -18,9 +18,16 @@ public record UserAccountDto(
         LocalDateTime modifiedAt,
         String modifiedBy
 ) {
+
+    // repository 레이어에서 JPA auditing에 의해 저장되기에 값을 설정하지 않아도 됨.
+    public static UserAccountDto of(String userId, String userPassword, String email, String nickname, String memo) {
+        return new UserAccountDto(userId, userPassword, email, nickname, memo, null, null, null, null);
+    }
+
     public static UserAccountDto of(String userId, String userPassword, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
         return new UserAccountDto(userId, userPassword, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
     }
+
     public static UserAccountDto from(UserAccount entity) {
         return new UserAccountDto(
                 entity.getUserId(),
